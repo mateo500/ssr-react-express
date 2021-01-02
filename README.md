0b6bf99f9406d59bf89fb647edca74ef175346cb
mateo guevara <mateogs1205@outlook.com>
2021-01-02 14:49:31 -0500

    load and stress tests added

--- README.md
+++ README.md
@@ -1,32 +1,34 @@
 # ssr-react-express
+
 ## Simple project with server side rendering(SSR) consuming a public API [Dog API](https://dog.ceo/dog-api/)
 
 ## Tech Stack:
+
 1. React
 2. Redux
 3. Stylus
 4. Webpack
 5. Express(SSR)
 
-
 ## Linting and formatting
+
 1. Eslint
 2. Prettier
 
-
 ## Testing
+
 1. React Testing Library & Jest -> src/tests/unit-testing
 2. Cypress(e2e/integration testing) -> cypress/integration
-
+3. Loadtest(stress and load tests) -> server/test
 
 ## npm commands
-1. run unit tests: ```npm run test```
-2. run integration tests and unit tests: ```npm run test-e2e```
-3. run the project in dev mode: ```npm run start-dev```
-4. run the project in production mode: ```npm run build && npm run start``` 
 
+1. run unit tests: `npm run test`
+2. run integration tests and unit tests: `npm run test-e2e`
+3. run stress and load tests: `npm run test-load`
+4. run the project in dev mode: `npm run start-dev`
+5. run the project in production mode: `npm run build && npm run start`
 
-### Deployment: 
+### Deployment:
+
 https://ssr-react-poc.herokuapp.com/
-
-
---
+++ server/test/loadingConnection.js
@@ -0,0 +1,43 @@
+/* eslint-disable consistent-return */
+/* eslint-disable func-names */
+const loadtest = require('loadtest');
+
+//10000 request per second, sacling concurrency in each request
+//this test are kind of slow to run so make sure to give it time, around 15 seconds on a 6 core machine
+
+loadtest.loadTest(
+  { url: 'http://localhost:5000', maxRequests: 10000, concurrency: 5 },
+  function (error, result) {
+    if (error) {
+      return console.error('Got an error: %s', error);
+    }
+    console.log('Tests run successfully');
+    console.log(result);
+  }
+);
+
+loadtest.loadTest(
+  {
+    url: 'http://localhost:5000/discover',
+    maxRequests: 10000,
+    concurrency: 10,
+  },
+  function (error, result) {
+    if (error) {
+      return console.error('Got an error: %s', error);
+    }
+    console.log('Tests run successfully');
+    console.log(result);
+  }
+);
+
+loadtest.loadTest(
+  { url: 'http://localhost:5000/manage', maxRequests: 10000, concurrency: 15 },
+  function (error, result) {
+    if (error) {
+      return console.error('Got an error: %s', error);
+    }
+    console.log('Tests run successfully');
+    console.log(result);
+  }
+);
