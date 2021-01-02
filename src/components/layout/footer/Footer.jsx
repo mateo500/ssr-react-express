2a6b99010e09510c14679e19438393cac0002011
mateo guevara <mateogs1205@outlook.com>
2021-01-02 16:31:21 -0500

    bug fixed in footer

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -5,12 +5,14 @@ export const Footer = () => {
   return (
     <footer>
       <div className='footer-content-container'>
-        <img
-          src='https://images.vexels.com/media/users/3/142912/isolated/preview/4e17695e6ff43707fa6f1078c24d99c8-black-dog-animal-logo-by-vexels.png'
-          alt='dog-logo-pic'
-          className='dog-logo-footer'
-        />
-        <h2>We hope you find your favorite dog!</h2>
+        <div>
+          <img
+            src='https://images.vexels.com/media/users/3/142912/isolated/preview/4e17695e6ff43707fa6f1078c24d99c8-black-dog-animal-logo-by-vexels.png'
+            alt='dog-logo-pic'
+            className='dog-logo-footer'
+          />
+          <h2 className='logo-title'>We hope you find your favorite dog!</h2>
+        </div>
         <h5 className='footer-message'>
           Remember to always adopt, don't buy, there's a little friend that may
           need your help, if you want to get involved, please visit{' '}
