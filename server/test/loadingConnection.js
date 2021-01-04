a722bbf7678e571b688907670afbbb0fe7db03f0
Mateo Guevara <59417295+mateo500@users.noreply.github.com>
2021-01-04 16:20:58 -0500

    Update loadingConnection.js

--- server/test/loadingConnection.js
+++ server/test/loadingConnection.js
@@ -2,11 +2,11 @@
 /* eslint-disable func-names */
 const loadtest = require('loadtest');
 
-//10000 request per test, scaling concurrency in each request x5, on different routes
+//5000 request per test, scaling concurrency in each request x5, on different routes
 //this test are kind of slow to run so make sure to give it time, around 15 seconds on a 6 core machine
 
 loadtest.loadTest(
-  { url: 'http://localhost:5000', maxRequests: 10000, concurrency: 5 },
+  { url: 'http://localhost:5000', maxRequests: 5000, concurrency: 5 },
   function (error, result) {
     if (error) {
       return console.error('Got an error: %s', error);
@@ -19,7 +19,7 @@ loadtest.loadTest(
 loadtest.loadTest(
   {
     url: 'http://localhost:5000/discover',
-    maxRequests: 10000,
+    maxRequests: 5000,
     concurrency: 10,
   },
   function (error, result) {
@@ -32,7 +32,7 @@ loadtest.loadTest(
 );
 
 loadtest.loadTest(
-  { url: 'http://localhost:5000/manage', maxRequests: 10000, concurrency: 15 },
+  { url: 'http://localhost:5000/manage', maxRequests: 5000, concurrency: 15 },
   function (error, result) {
     if (error) {
       return console.error('Got an error: %s', error);
